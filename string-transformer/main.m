bbc761439513b251e0cdda4c97549b5f64a05bee
snamiki1212 <snamiki1212@gmail.com>
2021-07-05 17:15:48 -0700

    Refactor brak line logic

--- string-transformer/main.m
+++ string-transformer/main.m
@@ -8,48 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
+// MARK: - NSString Helper
+NSString *sanitaizeBreakLine(NSString *str) {
+    return [str stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+}
+
 // MARK: - Input Helpers
 NSString *getInputStr(void) {
-    // 255 unit long array of characters
     char inputChars[255];
-
     printf("Input a string: ");
-    
-    // limit input to max 255 characters
     fgets(inputChars, 255, stdin);
-
-    // print as a c string
-    // printf("Your string is %s\n", inputChars);
-    
-    // convert char array to an NSString object
-    NSString *inputString = [NSString stringWithUTF8String:inputChars];
-    
-    // print NSString object
-    // NSLog(@"Input was: %@", inputString);
-    
-    // TODO: sanitaize \n
-    
+    NSString *inputString = sanitaizeBreakLine([NSString stringWithUTF8String:inputChars]);
     return inputString;
 }
 
 NSString *getInputMenuIndex(void) {
     char inputChars[255];
-
     printf("Input a Menu Number: ");
-    // limit input to max 255 characters
     fgets(inputChars, 255, stdin);
-
-    // print as a c string
-    // printf("Your string is %s\n", inputChars);
-    
-    // convert char array to an NSString object
-    NSString *inputString = [NSString stringWithUTF8String:inputChars];
-
-    // print NSString object
-    // NSLog(@"Input was: %@", inputString);
-    
-    // TODO: sanitaize \n
-
+    NSString *inputString = sanitaizeBreakLine([NSString stringWithUTF8String:inputChars]);
     return inputString;
 }
 
@@ -58,12 +35,12 @@ NSString *getInputMenuIndex(void) {
 BOOL validateMenuIndex(NSString *maybeMenuIndex) {
     // TODO: refactor like [1,2,3,...].includes(idx)
     return(
-           [maybeMenuIndex isEqual:@"1\n"] ||
-           [maybeMenuIndex isEqual:@"2\n"] ||
-           [maybeMenuIndex isEqual:@"3\n"] ||
-           [maybeMenuIndex isEqual:@"4\n"] ||
-           [maybeMenuIndex isEqual:@"5\n"] ||
-           [maybeMenuIndex isEqual:@"6\n"]
+           [maybeMenuIndex isEqual:@"1"] ||
+           [maybeMenuIndex isEqual:@"2"] ||
+           [maybeMenuIndex isEqual:@"3"] ||
+           [maybeMenuIndex isEqual:@"4"] ||
+           [maybeMenuIndex isEqual:@"5"] ||
+           [maybeMenuIndex isEqual:@"6"]
            );
 }
 
@@ -72,11 +49,11 @@ BOOL canNumberize(NSString *maybeNumberableVal) {
 }
 
 BOOL isQuestion(NSString *str) {
-    return [str hasSuffix:@"?\n"];
+    return [str hasSuffix:@"?"];
 }
 
 BOOL isProtestation(NSString *str) {
-    return [str hasSuffix:@"!\n"];
+    return [str hasSuffix:@"!"];
 }
 
 int numberize(NSString *str) {
@@ -104,13 +81,13 @@ void handleNumberize(NSString *str) {
     if(canNumberize(str)) {
         printf("%d", numberize(str));
     } else {
-        printf("Invalid value. Your input string cannot convert into int.");
+        printf("Your input value cannot be numberized.");
     }
     return;
 }
 
 void handleCanadianize(NSString *str) {
-    NSString *newStr = [NSString stringWithFormat:@"%@, eh?", str];
+    NSString *newStr = [NSString stringWithFormat:@"%@, eh?", sanitaizeBreakLine(str)];
     printNSString(newStr);
     return;
 }
@@ -133,12 +110,12 @@ void handleDoSpaceIt(NSString *str) {
 }
 
 void *callMenu(NSString *menuIdx, NSString *str) {
-    if([menuIdx isEqual:@"1\n"]) handleUppercase(str);
-    if([menuIdx isEqual:@"2\n"]) handleLowercase(str);
-    if([menuIdx isEqual:@"3\n"]) handleNumberize(str);
-    if([menuIdx isEqual:@"4\n"]) handleCanadianize(str);
-    if([menuIdx isEqual:@"5\n"]) handleRespond(str);
-    if([menuIdx isEqual:@"6\n"]) handleDoSpaceIt(str);
+    if([menuIdx isEqual:@"1"]) handleUppercase(str);
+    if([menuIdx isEqual:@"2"]) handleLowercase(str);
+    if([menuIdx isEqual:@"3"]) handleNumberize(str);
+    if([menuIdx isEqual:@"4"]) handleCanadianize(str);
+    if([menuIdx isEqual:@"5"]) handleRespond(str);
+    if([menuIdx isEqual:@"6"]) handleDoSpaceIt(str);
     return NULL;
 }
 
